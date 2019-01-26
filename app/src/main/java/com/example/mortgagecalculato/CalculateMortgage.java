90399c5d8dcd5aa255cc335b217d8b9748677659
Stephanie Giang <stephaniegiang1@gmail.com>
2019-01-26 16:57:48 -0500

    Round result to 2 decimals

--- app/src/main/java/com/example/mortgagecalculato/CalculateMortgage.java
+++ app/src/main/java/com/example/mortgagecalculato/CalculateMortgage.java
@@ -1,11 +1,14 @@
 package com.example.mortgagecalculato;
 
 public class CalculateMortgage {
+    final int monthlyPayment = 12;
+
     static public double calculate (int principalAmount, double interest, int totalPeriod){
         int totalPayments = totalPeriod * 12;
-        double monthly_interest = (interest/100)/12;
+        double monthlyInterest = (interest/100)/12;
 
-        double interestPortion = Math.pow(1+monthly_interest, totalPayments);
-        return principalAmount * ((monthly_interest * interestPortion )/(interestPortion - 1));
+        double interestPortion = Math.pow(1+monthlyInterest, totalPayments);
+        double result = principalAmount * ((monthlyInterest * interestPortion )/(interestPortion - 1));
+        return Math.round(result * 100.00)/100.00;
     }
 }
