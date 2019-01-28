0d45b1290fb6a0e06b155e8596ef29714a3ef537
Projudah <projudah@gmail.com>
2019-01-28 17:03:24 -0500

    help image

--- app/src/main/java/com/example/mortgagecalculato/MainActivity.java
+++ app/src/main/java/com/example/mortgagecalculato/MainActivity.java
@@ -2,6 +2,8 @@ package com.example.mortgagecalculato;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.EditText;
@@ -32,5 +34,26 @@ public class MainActivity extends AppCompatActivity {
                 Toast.makeText(getApplicationContext(), result, Toast.LENGTH_LONG).show();
             }
         });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the main_menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        switch(item.getItemId()) {
+            case R.id.help:
+                //Steph: this is where the code for the help goes to
+                break;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+
+        return true;
     }
 }
